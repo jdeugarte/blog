736383075d5127b5adde513aafc543c4f4b0319a
Jorge <jorge@jorge-Inspiron-N5110.(none)>
2014-04-03 22:12:01 -0400

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,68 @@
+class PostsController < ApplicationController
+	skip_before_filter :verify_authenticity_token 
+	
+	def index
+		@posts=Post.order(:created_at)
+	end
+
+	def crear
+		
+	end
+
+	def show
+		@post=Post.find(params[:id])
+		
+	end
+
+	def reporte
+		@posts=Post.all
+	end
+	
+	def liked
+		@post = Post.find(params[:id])
+		a=@post.likes
+		@post.likes=a+1
+		@post.save
+		redirect_to('/posts/show/'+params[:id])
+	end
+	
+	def search
+			
+	end	
+	
+	def create
+		@post = Post.new(post_params)
+		@post.likes=0
+		@post.save
+		redirect_to("/posts")
+	end
+
+	def delete
+		@post=Post.find(params[:id])
+		@post.delete
+		redirect_to("/posts")
+	end
+
+	def update
+		@post=Post.find(params[:id])
+	end
+
+
+	def edit
+		@post=Post.find(params[:id])
+		@post.update(params[:post].permit(:title, :text))
+
+		redirect_to("/posts")
+	end
+	
+	def like
+		@post=Post.find(params[:id])
+		@post.likes=@post.likes+1
+		@post.save
+		redirect_to("/posts")
+	end
+	
+	def post_params
+		params.require(:post).permit(:title, :text)
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'a0e9e209d61a5a62bb785fc409d0c3c38619625fcd1aac5b9c865f0f791afb79619f2571eff43d24b7f2d4ee4735ff5d9ae542df5e751cd0d33a0144bfd27555'
