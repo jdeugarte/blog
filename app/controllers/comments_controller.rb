6af93f7634f8e8b68f5434e1feaaadebcb936c9f
Jorge <jorge@jorge-Inspiron-N5110.(none)>
2014-04-10 14:52:40 -0400

    Examen(Version Final)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
 def create
-   
+    @post = Post.find(params[:id])
+    @ = @post.comments.create(params[:comment].permit(:commenter, :body))
+    redirect_to '/posts/index'
   end
 
 end
