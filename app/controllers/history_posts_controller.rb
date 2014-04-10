17ad993525876d589e2848c7dc111f925f1a43d9
Jorge <jorge@jorge-Inspiron-N5110.(none)>
2014-04-10 14:49:37 -0400

    Examen(Version Final)

--- app/controllers/history_posts_controller.rb
+++ app/controllers/history_posts_controller.rb
@@ -1,2 +1,9 @@
 class HistoryPostsController < ApplicationController
+
+	def create
+    @post = Post.find(params[:post_id])
+    @hystory_post = @post.hystory_post.create(params[:comment].permit(:commenter, :body))
+    redirect_to post_path(@post)
+  end
+
 end
